231cde8106f44c5ae038470485f0c26293b312ce
Gabriel4g <gabrielprogramador11@gmail.com>
2022-07-31 17:07:46 -0500

    edit functions

--- js/main.js
+++ js/main.js
@@ -2,34 +2,41 @@ const stringModes = ["Light", "Dark"];
 const iconSol = document.querySelector("#sol");
 const iconLua = document.querySelector("#lua");
 let p = document.querySelector(".paragrafo-invisivel");
-
 const instaName = document.querySelector(".instaName")
 
 function animation(rotateAnimation) {
     return rotateAnimation.style.animation = "rotate 900ms ease-in-out"
 }
 
-function Modes() {
+function darkTheme() {
     const name = document.querySelector(".name");
+    name.style.color = "#fff"
+    instaName.style.color = "#fff"
+    document.body.classList.add("darkMode");
+    document.body.classList.remove("lightMode");
+    iconLua.style.display = "none";
+    iconSol.style.display = "block";
+    p.innerHTML = stringModes[0];
+}
 
+function lightTheme() {
+    const name = document.querySelector(".name");
+    name.style.color = "#0e091b";
+    instaName.style.color = "#0e091b"
+    document.body.classList.add("lightMode");
+    iconSol.style.display = "none";
+    iconLua.style.display = "block";
+    p.innerHTML = stringModes[1];
+}
+
+function lightAndDark() {
     if (p.innerHTML == stringModes[0]) {
-        name.style.color = "#0e091b";
-        instaName.style.color = "#0e091b"
-        document.body.classList.add("lightMode");
-        iconSol.style.display = "none";
-        iconLua.style.display = "block";
+        lightTheme()
         animation(iconLua)
-        p.innerHTML = stringModes[1];
     }
 
     else if (p.innerHTML == stringModes[1]) {
-        name.style.color = "#fff"
-        instaName.style.color = "#fff"
-        document.body.classList.add("darkMode");
-        document.body.classList.remove("lightMode");
-        iconLua.style.display = "none";
-        iconSol.style.display = "block";
-        p.innerHTML = stringModes[0];
+        darkTheme()
         animation(iconSol);
     }
 }
