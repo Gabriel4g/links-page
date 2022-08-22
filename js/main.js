101f58e3b4537cb549a71232fa5aabc28a1b41d2
Gabriel4g <gabrielprogramador11@gmail.com>
2022-08-21 23:21:33 -0500

    edit themes function

--- js/main.js
+++ js/main.js
@@ -1,42 +1,30 @@
-const stringModes = ["Light", "Dark"];
-const iconSol = document.querySelector("#sol");
-const iconLua = document.querySelector("#lua");
-let p = document.querySelector(".paragrafo-invisivel");
+const iconsTheme = document.querySelector("#img-theme");
 const instaName = document.querySelector(".instaName")
+const myName = document.querySelector(".name");
+const projectText = document.querySelector(".h2-projects");
 
 function animation(rotateAnimation) {
-    return rotateAnimation.style.animation = "rotate 900ms ease-in-out"
-}
-
-function darkTheme() {
-    const name = document.querySelector(".name");
-    name.style.color = "#fff"
-    instaName.style.color = "#fff"
-    document.body.classList.add("darkMode");
-    document.body.classList.remove("lightMode");
-    iconLua.style.display = "none";
-    iconSol.style.display = "block";
-    p.innerHTML = stringModes[0];
-}
-
-function lightTheme() {
-    const name = document.querySelector(".name");
-    name.style.color = "#0e091b";
-    instaName.style.color = "#0e091b"
-    document.body.classList.add("lightMode");
-    iconSol.style.display = "none";
-    iconLua.style.display = "block";
-    p.innerHTML = stringModes[1];
+    rotateAnimation.style.animation = ""
+    setTimeout(() => {
+        rotateAnimation.style.animation = "rotate 900ms ease-in-out"
+    }, 100)
 }
 
 function lightAndDark() {
-    if (p.innerHTML == stringModes[0]) {
-        lightTheme()
-        animation(iconLua)
+    animation(iconsTheme)
+    const toggle = document.body.classList.toggle("lightMode");
+    if (!toggle) {
+        instaName.style.color = "#fff"
+        myName.style.color = "#fff"
+        projectText.style.color = "#fff"
+        iconsTheme.src = "./assets/icon-sol.png"
     }
 
-    else if (p.innerHTML == stringModes[1]) {
-        darkTheme()
-        animation(iconSol);
+    else {
+        instaName.style.color = "#000"
+        myName.style.color = "#000"
+        projectText.style.color = "#000"
+        document.body.classList.add("darkMode")
+        iconsTheme.src = "./assets/lua-icon.png"
     }
 }
